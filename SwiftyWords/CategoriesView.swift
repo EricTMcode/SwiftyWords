b18bb83daa3d2fb93972f2fa66f2ca47981be3fb
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-30 15:54:48 +0100

    Create CategoriesView

--- SwiftyWords/CategoriesView.swift
+++ SwiftyWords/CategoriesView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct CategoriesView: View {
+    var categories = Bundle.main.decode([String].self, from: "Categories.json").map {
+        Bundle.main.decode(Category.self, from: "\($0).json")
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(categories) { category in
+                VStack(alignment: .leading) {
+                    Text(category.name)
+                        .font(.headline)
+
+                    Text(category.description)
+                }
+            }
+        }
+        .navigationDestination(for: Category.self) { category in
+            LevelsView(category: category)
+        }
+        .navigationTitle("7 Swifty Words")
     }
 }
 
