aece2d8f2b71f8a79e3ebffa7a44bb14de5b5602
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-30 13:58:38 +0100

    Initial Commit

---
+++ SwiftyWords/SwiftyWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWordsApp.swift
+//  SwiftyWords
+//
+//  Created by Eric on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
